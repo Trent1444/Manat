f622221f9f3a19b91e88885ddd1d461de05239f7
TrentDev <TrentLathim@gmail.com>
2022-06-22 16:17:47 -0700

    Added Cinemachine to project

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+	public GameObject CinemachineCameraTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
